939539b542bb0c17b1a750901ec8ccb8d2e2814b
Qhizer Shareef <51617074+qhizershareef@users.noreply.github.com>
2020-03-22 00:33:00 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,14 +4,7 @@ let final_date=d.getDate()+"-"+d.getMonth()+"-"+d.getFullYear();
 console.log (final_date);
 
   var firebaseConfig = {
-    apiKey: "AIzaSyBNmtWamIMlz2vsHkH_J5oa1HLv05SV6_Y",
-    authDomain: "salah-count.firebaseapp.com",
-    databaseURL: "https://salah-count.firebaseio.com",
-    projectId: "salah-count",
-    storageBucket: "salah-count.appspot.com",
-    messagingSenderId: "851272485923",
-    appId: "1:851272485923:web:8175f2f8567dfc02d33596",
-    measurementId: "G-N5QNLK45XJ"
+/**  Add Your firebase config which is provided by firebase **/
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
@@ -20,7 +13,6 @@ console.log (final_date);
 
 
 function addInToFirebase(temparr){
-    //var newMessageRef= messageref.child(final_date).push();
     var newMessageRef= messageref.push();
     newMessageRef.update({
         Date:final_date,
